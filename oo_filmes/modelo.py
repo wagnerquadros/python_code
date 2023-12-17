698d28234cedd46bd143600514a4c2070cf3479b
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2023-12-17 09:21:23 -0300

    Herança

--- oo_filmes/modelo.py
+++ oo_filmes/modelo.py
@@ -1,20 +1,52 @@
-class Filme:
-
-    def __init__(self, nome, ano, duracao):
-        self.nome = nome
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
         self.ano = ano
+        self._likes = 0
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def likes(self):
+        return self._likes
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, noveo_nome):
+        self.__nome = noveo_nome.title()
+
+#================================
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
         self.duracao = duracao
 
-class Serie:
 
-    def __init__(self, nome, ano, temporadas):
-        self.nome = nome
-        self.ano = ano
+
+#================================
+
+class Serie(Programa):
+    def __init__(self, nome, ano,  temporadas):
+        super().__init__(nome, ano)
         self.temporadas = temporadas
 
 
-filme = Filme('Matrix', 2244, 123)
-print(f'Nome: {filme.nome} - Ano: {filme.ano} - Duracao: {filme.duracao}')
 
-serie = Serie('Atlanta', 2017, 3)
-print(f'Nome: {serie.nome} - Ano: {serie.ano} - Temporadas: {serie.temporadas}')
+# -------------------------------------------------------------------
+
+filme = Filme('matrix', 2244, 123)
+
+print(f'Nome: {filme.nome} - Ano: {filme.ano} - Duracao: {filme.duracao} '
+      f'- Likes: {filme.likes}')
+
+serie = Serie('atlanta', 2017, 3)
+serie.dar_like()
+print(f'Nome: {serie.nome} - Ano: {serie.ano} - Temporadas: {serie.temporadas} '
+      f'- Likes: {serie.likes}')
+
+
