f3fa45e4943fe9b6a4dc543c1211e4eb1bb1e157
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2023-12-26 19:55:56 -0300

    class Playlist

--- oo_filmes/modelo.py
+++ oo_filmes/modelo.py
@@ -43,21 +43,51 @@ class Serie(Programa):
     def __str__(self):
         return f'{self._nome} - {self.ano} - {self._likes} - Temporadas: {self.temporadas}'
 
+#================================
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
+
 
 
 # -------------------------------------------------------------------
 
 filme = Filme('matrix', 2244, 123)
+serie = Serie('atlanta', 2017, 3)
+tmep = Filme('Todo mundo em panico', 2001, 120)
+lost = Serie('Lost', 2005, 7)
+
+serie.dar_like()
+tmep.dar_like()
+lost.dar_like()
+filme.dar_like()
 
 print(f'Nome: {filme.nome} - Ano: {filme.ano} - Duracao: {filme.duracao} '
       f'- Likes: {filme.likes}')
 
-serie = Serie('atlanta', 2017, 3)
 serie.dar_like()
 print(f'Nome: {serie.nome} - Ano: {serie.ano} - Temporadas: {serie.temporadas} '
       f'- Likes: {serie.likes}')
 
-filmes_e_series = [filme,serie]
+filmes_e_series = [filme,serie, tmep, lost]
+playlist_fim_de_semana = Playlist('Fim de Semana', filmes_e_series)
 
-for programa in filmes_e_series:
+for programa in playlist_fim_de_semana:
     print(programa)
+
+
+print(f'Lost está na playlist? {lost in playlist_fim_de_semana}')
