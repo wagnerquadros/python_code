85fbc2f41eba33736f3cb04a99fdb86d09d45b7a
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2023-12-31 08:27:02 -0300

    Validação da opção escolhida

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -12,23 +12,34 @@ def exibir_opcoes():
     print('4. Sair\n')
 
 def finalizar_app():
-    os.system('cls')
+    os.system('cls') # Limpa o console
     print('Fnalizando o app\n')
 
-def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    print(f'Você escolheu a opção: {opcao_escolhida}!')
+def opcao_invalida():
+    print('Opção Inválida')
+    input('Precione uma tecla para votas ao menu')
+    main()
 
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar Restaurante')
-    elif opcao_escolhida == 3:
-        print('Ativar Restaurante')
-    else:
-        finalizar_app()
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        print(f'Você escolheu a opção: {opcao_escolhida}!')
+
+        if opcao_escolhida == 1:
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print('Listar Restaurante')
+        elif opcao_escolhida == 3:
+            print('Ativar Restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('cls')
     exibir_nome()
     exibir_opcoes()
     escolher_opcao()
